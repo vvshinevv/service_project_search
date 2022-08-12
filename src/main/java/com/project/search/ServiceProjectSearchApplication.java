0c605b59b3f53b9a69fa78d5263bcb236a7ab384
최홍희 <vvshinevv@nhnsoft.com>
2022-08-13 00:53:45 +0900

    initialize project

---
+++ src/main/java/com/project/search/ServiceProjectSearchApplication.java
@@ -0,0 +1,13 @@
+package com.project.search;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceProjectSearchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServiceProjectSearchApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/search/ServiceProjectSearchApplicationTests.java
@@ -0,0 +1,17 @@
+package com.project.search;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceProjectSearchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+	@Test
+	void simpleTest() {
+		System.out.println("simple test");
+	}
+}
